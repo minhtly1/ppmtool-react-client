9ec2ae0c5301b52a607757335b72bb706b35a91c
Minh Ly <minhtly1@gmail.com>
2019-08-17 18:20:52 -0400

    Create addProject project and routing it up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import "./App.css";
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from './components/Project/AddProject';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Header />
-				<Dashboard />
-			</div>
+			//React router that route to this file when hit home address
+			<Router>
+				<div className="App">
+					<Header />
+					<Dashboard />
+					<Route exact path="/dashboard" component={Dashboard}/>
+					<Route exact path="/addProject" component={AddProject}/>
+				</div>
+			</Router>
 		);
 	}
 }

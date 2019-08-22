3e37b76a2b59d70e8ea06fe9309abe3e5afbc5ed
Minh Ly <minhtly1@gmail.com>
2019-08-21 23:52:19 -0400

    got front end to POST to db

--- src/components/Project/AddProject.js
+++ src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types"
+import {connect} from "react-redux"
+import {createProject} from "../../actions/projectActions";
 
 class AddProject extends Component {
 	//create domain object constructors
@@ -33,9 +36,10 @@ class AddProject extends Component {
 			"description": this.state.description,
 			"start_date": this.state.start_date,
 			"end_date": this.state.end_date,
-		}
+		}; 
 
-		console.log(newProject);
+		this.props.createProject(newProject, this.props.history)
+
 	}
 
 	render() {
@@ -80,4 +84,8 @@ class AddProject extends Component {
 	}
 }
 
-export default AddProject;
+AddProject.propTypes = {
+	createProject: PropTypes.func.isRequired
+}
+
+export default connect(null, {createProject}) (AddProject);
